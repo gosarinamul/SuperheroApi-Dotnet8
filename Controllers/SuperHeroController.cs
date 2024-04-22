b84aefe5654f398cbe8782ee8b3d41365c2f719f
seul7i <seulgi.lee01@student.weltec.ac.nz>
2024-04-22 19:44:34 +1200

    create a controller and add GET method

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperheroApi_Dotnet8.Controllers.Entities;
+
+namespace SuperheroApi_Dotnet8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New york city"
+                }
+            };
+            return Ok(heroes);
+        
+
+    }
+}
