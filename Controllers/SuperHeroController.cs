d5d43a73494d7908e94bae5f048eb47c585a4589
seul7i <seulgi.lee01@student.weltec.ac.nz>
2024-04-22 21:24:46 +1200

    Add the rest of CRUD methods

--- Controllers/SuperHeroController.cs
+++ Controllers/SuperHeroController.cs
@@ -26,5 +26,45 @@ namespace SuperheroApi_Dotnet8.Controllers
         
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetHero(int id)
+        {
+            // DB set of SuperHeroes
+            var hero = await _context.SuperHeroes.FindAsync(id);
+            if (hero == null)
+                return BadRequest("Hero not found with id " + id);
+            
+            return Ok(hero);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero([FromBody]SuperHero hero)
+        {
+            // DB set of SuperHeroes
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero([FromBody] SuperHero hero)
+        {
+            var dbHero = await _context.SuperHeroes.FindAsync(hero.Id);
+            if (dbHero == null)
+                return BadRequest("Hero not found with id " + hero.Id);
+
+            dbHero.Name = hero.Name;
+            dbHero.FirstName = hero.FirstName;
+            dbHero.LastName = hero.LastName;
+            dbHero.Place = hero.Place;
+
+            await _context.SaveChangesAsync();
+            
+            return Ok(await _context.SuperHeroes.ToListAsync());
+
+        }
     }
 }
