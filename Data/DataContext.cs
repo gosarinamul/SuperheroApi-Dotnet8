1024f55cd3a2b0aa55c3835b65bd6a72555d91d6
seul7i <seulgi.lee01@student.weltec.ac.nz>
2024-04-22 20:55:20 +1200

    Add db context

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,8 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SuperheroApi_Dotnet8.Controllers.Entities;
 
 namespace SuperheroApi_Dotnet8.Data
 {
     public class DataContext : DbContext
     {
+        // Boiler plate code
+        // run base construction with options
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        // Add database set
+        public DbSet<SuperHero> SuperHeroes { get; set; }
     }
 }
